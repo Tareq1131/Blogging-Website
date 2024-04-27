57d91644d6fd072dddd0efbd849403d0ee1f68b3
Tareq1131 <tareqislam076@gmail.com>
2024-04-27 21:49:55 +0600

    Login With Email And Password

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
+  Navigate,
 } from "react-router-dom";
 import Home from './pages/home/Home';
 import Blog from './pages/blog/Blog';
@@ -28,8 +29,16 @@ function App() {
           <Route path="/allblogs" element={<AllBlogs />} />
           <Route path="/bloginfo/:id" element={<BlogInfo />} />
           <Route path="/adminlogin" element={<AdminLogin />} />
-          <Route path="/dashboard" element={<DashBoard />} />
-          <Route path="/createblog" element={<CreateBlog />} />
+          <Route path="/dashboard" element={
+            <ProtectedRouteForAdmin>
+              <DashBoard />
+            </ProtectedRouteForAdmin>
+          } />
+          <Route path="/createblog" element={
+            <ProtectedRouteForAdmin>
+              <CreateBlog />
+            </ProtectedRouteForAdmin>
+          } />
           <Route path="/*" element={<NoPage />} />
         </Routes>
         <Toaster/>
@@ -39,3 +48,14 @@ function App() {
 }
 
 export default App
+
+// eslint-disable-next-line react/prop-types
+export const ProtectedRouteForAdmin = ({ children }) => {
+  const admin = JSON.parse(localStorage.getItem('admin'))
+  if (admin?.user?.email === "testuser@gmail.com") {
+    return children
+  }
+  else {
+    return <Navigate to={'/adminlogin'}/>
+  }
+}
