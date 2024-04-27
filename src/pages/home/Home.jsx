343ea4661557de941e56274f6ded2b43eff86eb8
Tareq1131 <tareqislam076@gmail.com>
2024-04-27 16:28:18 +0600

    create components and pages

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
+import BlogPostCard from "../../components/blogPostCard/BlogPostCard";
+import HeroSection from "../../components/heroSection/HeroSection";
+import Layout from "../../components/layout/Layout";
 
 
 function Home() {
   return (
-    <div>
-      Home
-    </div>
-  )
+    <Layout>
+      {" "}
+      <HeroSection />
+      <BlogPostCard />
+      
+    </Layout>
+  );
 }
 
-export default Home
+export default Home;
