eebf1fc6ebdfbaff3b225941ff6ac481e3ba303e
Tareq1131 <tareqislam076@gmail.com>
2024-04-27 22:17:31 +0600

    create blog post

--- src/pages/admin/createBlog/CreateBlog.jsx
+++ src/pages/admin/createBlog/CreateBlog.jsx
@@ -1,204 +1,291 @@
-import  { useState, useContext } from 'react';
-import { Editor } from '@tinymce/tinymce-react';
-import { BsFillArrowLeftCircleFill } from "react-icons/bs"
-import myContext from '../../../context/data/myContext';
-import { Link } from "react-router-dom";
-import {
-    Button,
-    Typography,
-} from "@material-tailwind/react";
+import { useState, useContext } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { BsFillArrowLeftCircleFill } from "react-icons/bs";
+import myContext from "../../../context/data/myContext";
+import { Link, useNavigate } from "react-router-dom";
+import { Button, Typography } from "@material-tailwind/react";
+import { Timestamp, addDoc, collection } from "firebase/firestore";
+import toast from "react-hot-toast";
+import { fireDB, storage } from "../../../fairebase/FirebaseConfig";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 function CreateBlog() {
-    const context = useContext(myContext);
-    const { mode } = context;
+  const context = useContext(myContext);
+  const { mode } = context;
 
-    const [blogs, setBlogs] = useState('');
-    const [thumbnail, setthumbnail] = useState();
+  const [blogs, setBlogs] = useState({
+    title: "",
+    category: "",
+    content: "",
+    time: Timestamp.now(),
+  });
 
-    const [text, settext] = useState('');
-    console.log("Value: ",);
-    console.log("text: ", text);
+  const [thumbnail, setthumbnail] = useState();
 
-     console.log(blogs)
+  const [text, settext] = useState("");
+  console.log("Value: ");
+  console.log("text: ", text);
+  const navigate = useNavigate();
 
-    // Create markup function 
-    function createMarkup(c) {
-        return { __html: c };
+  console.log(blogs);
+
+  const addPost = async () => {
+    if (
+      blogs.title === "" ||
+      blogs.category === "" ||
+      blogs.content === "" ||
+      blogs.thumbnail === ""
+    ) {
+      toast.error("Please Fill All Fields");
     }
-    return (
-        <div className=' container mx-auto max-w-2xl py-5'>
-            <div className="p-5" style={{
-                background: mode === 'dark'
-                    ? '#353b48'
-                    : 'rgb(226, 232, 240)',
-                borderBottom: mode === 'dark'
-                    ? ' 4px solid rgb(226, 232, 240)'
-                    : ' 4px solid rgb(30, 41, 59)'
-            }}>
-                {/* Top Item  */}
-                <div className="mb-2 flex justify-between">
-                    <div className="flex gap-2 items-center">
-                        {/* Dashboard Link  */}
-                        <Link to={'/dashboard'}>
-                            <BsFillArrowLeftCircleFill size={25} />
-                        </Link>
+    // console.log(blogs.content)
+    uploadImage();
+  };
 
-                        {/* Text  */}
-                        <Typography
-                            variant="h4"
-                            style={{
-                                color: mode === 'dark'
-                                    ? 'white'
-                                    : 'black'
-                            }}
-                        >
-                            Create blog
-                        </Typography>
-                    </div>
-                </div>
+  const uploadImage = () => {
+    if (!thumbnail) return;
+    const imageRef = ref(storage, `blogimage/${thumbnail.name}`);
+    uploadBytes(imageRef, thumbnail).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        const productRef = collection(fireDB, "blogPost");
+        try {
+          addDoc(productRef, {
+            blogs,
+            thumbnail: url,
+            time: Timestamp.now(),
+            date: new Date().toLocaleString("en-US", {
+              month: "short",
+              day: "2-digit",
+              year: "numeric",
+            }),
+          });
+          navigate("/dashboard");
+          toast.success("Post Added Successfully");
+        } catch (error) {
+          toast.error(error);
+          console.log(error);
+        }
+      });
+    });
+  };
 
-                {/* main Content  */}
-                <div className="mb-3">
-                    {/* Thumbnail  */}
-                    {thumbnail && <img className=" w-full rounded-md mb-3 "
-                        src={thumbnail
-                            ? URL.createObjectURL(thumbnail)
-                            : ""}
-                        alt="thumbnail"
-                    />}
+  // Create markup function
+  function createMarkup(c) {
+    return { __html: c };
+  }
+  return (
+    <div className=" container mx-auto max-w-2xl py-5">
+      <div
+        className="p-5"
+        style={{
+          background: mode === "dark" ? "#353b48" : "rgb(226, 232, 240)",
+          borderBottom:
+            mode === "dark"
+              ? " 4px solid rgb(226, 232, 240)"
+              : " 4px solid rgb(30, 41, 59)",
+        }}
+      >
+        {/* Top Item  */}
+        <div className="mb-2 flex justify-between">
+          <div className="flex gap-2 items-center">
+            {/* Dashboard Link  */}
+            <Link to={"/dashboard"}>
+              <BsFillArrowLeftCircleFill size={25} />
+            </Link>
 
-                    {/* Text  */}
-                    <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="mb-2 font-semibold"
-                        style={{ color: mode === 'dark' ? 'white' : 'black' }}
-                    >
-                        Upload Thumbnail
-                    </Typography>
+            {/* Text  */}
+            <Typography
+              variant="h4"
+              style={{
+                color: mode === "dark" ? "white" : "black",
+              }}
+            >
+              Create blog
+            </Typography>
+          </div>
+        </div>
 
-                    {/* First Thumbnail Input  */}
-                    <input
-                        type="file"
-                        label="Upload thumbnail"
-                        className="shadow-[inset_0_0_4px_rgba(0,0,0,0.6)] placeholder-black w-full rounded-md p-1"
-                        style={{
-                            background: mode === 'dark'
-                                ? '#dcdde1'
-                                : 'rgb(226, 232, 240)'
-                        }}
-                        onChange={(e) => setthumbnail(e.target.files[0])}
-                    />
-                </div>
+        {/* main Content  */}
+        <div className="mb-3">
+          {/* Thumbnail  */}
+          {thumbnail && (
+            <img
+              className=" w-full rounded-md mb-3 "
+              src={thumbnail ? URL.createObjectURL(thumbnail) : ""}
+              alt="thumbnail"
+            />
+          )}
 
-                {/* Second Title Input */}
-                <div className="mb-3">
-                    <input
-                        label="Enter your Title"
-                       className={`shadow-[inset_0_0_4px_rgba(0,0,0,0.6)] w-full rounded-md p-1.5 
-                 outline-none ${mode === 'dark'
-                 ? 'placeholder-black'
-                 : 'placeholder-black'}`}
-                        placeholder="Enter Your Title"
-                        style={{
-                            background: mode === 'dark'
-                                ? '#dcdde1'
-                                : 'rgb(226, 232, 240)'
-                        }}
-                        name="title"
-                    />
-                </div>
+          {/* Text  */}
+          <Typography
+            variant="small"
+            color="blue-gray"
+            className="mb-2 font-semibold"
+            style={{ color: mode === "dark" ? "white" : "black" }}
+          >
+            Upload Thumbnail
+          </Typography>
 
-                {/* Third Category Input  */}
-                <div className="mb-3">
-                    <input
-                        label="Enter your Category"
-                        className={`shadow-[inset_0_0_4px_rgba(0,0,0,0.6)] w-full rounded-md p-1.5 
-                 outline-none ${mode === 'dark'
-                 ? 'placeholder-black'
-                 : 'placeholder-black'}`}
-                        placeholder="Enter Your Category"
-                        style={{
-                            background: mode === 'dark'
-                                ? '#dcdde1'
-                                : 'rgb(226, 232, 240)'
-                        }}
-                        name="category"
-                    />
-                </div>
+          {/* First Thumbnail Input  */}
+          <input
+            type="file"
+            label="Upload thumbnail"
+            className="shadow-[inset_0_0_4px_rgba(0,0,0,0.6)] placeholder-black w-full rounded-md p-1"
+            style={{
+              background: mode === "dark" ? "#dcdde1" : "rgb(226, 232, 240)",
+            }}
+            onChange={(e) => setthumbnail(e.target.files[0])}
+          />
+        </div>
 
-                {/* Four Editor  */}
-                <Editor
-                    apiKey='zsnrfchd5qd7ri7ltirhv1lcglmq59yhtexly7ospxh3vq2d'
-                    onEditorChange={(newValue, editor) => {
-                        setBlogs({ blogs, content: newValue });
-                        settext(editor.getContent({ format: 'text' }));
-                    }}
-                    onInit={(evt, editor) => {
-                        settext(editor.getContent({ format: 'text' }));
-                    }}
-                    init={{
-                        plugins: 'a11ychecker advcode advlist advtable anchor autocorrect autolink autoresize autosave casechange charmap checklist code codesample directionality editimage emoticons export footnotes formatpainter fullscreen help image importcss inlinecss insertdatetime link linkchecker lists media mediaembed mentions mergetags nonbreaking pagebreak pageembed permanentpen powerpaste preview quickbars save searchreplace table tableofcontents template  tinydrive tinymcespellchecker typography visualblocks visualchars wordcount'
-                    }}
-                />
+        {/* Second Title Input */}
+        <div className="mb-3">
+          <input
+            label="Enter your Title"
+            className={`shadow-[inset_0_0_4px_rgba(0,0,0,0.6)] w-full rounded-md p-1.5 
+                 outline-none ${
+                   mode === "dark" ? "placeholder-black" : "placeholder-black"
+                 }`}
+            placeholder="Enter Your Title"
+            style={{
+              background: mode === "dark" ? "#dcdde1" : "rgb(226, 232, 240)",
+            }}
+            name="title"
+            value={blogs.title}
+            onChange={(e) => setBlogs({ ...blogs, title: e.target.value })}
+          />
+        </div>
 
-                {/* Five Submit Button  */}
-                <Button className=" w-full mt-5"
-                    style={{
-                        background: mode === 'dark'
-                            ? 'rgb(226, 232, 240)'
-                            : 'rgb(30, 41, 59)',
-                        color: mode === 'dark'
-                            ? 'rgb(30, 41, 59)'
-                            : 'rgb(226, 232, 240)'
-                    }}
-                >
-                    Send
-                </Button>
+        {/* Third Category Input  */}
+        <div className="mb-3">
+          <input
+            label="Enter your Category"
+            className={`shadow-[inset_0_0_4px_rgba(0,0,0,0.6)] w-full rounded-md p-1.5 
+                 outline-none ${
+                   mode === "dark" ? "placeholder-black" : "placeholder-black"
+                 }`}
+            placeholder="Enter Your Category"
+            style={{
+              background: mode === "dark" ? "#dcdde1" : "rgb(226, 232, 240)",
+            }}
+            name="category"
+            onChange={(e) => setBlogs({ ...blogs, category: e.target.value })}
+            value={blogs.category}
+          />
+        </div>
 
-                {/* Six Preview Section  */}
-                <div className="">
-                    <h1 className=" text-center mb-3 text-2xl">Preview</h1>
-                    <div className="content">
-                    <div
-                        className={`[&> h1]:text-[32px] [&>h1]:font-bold  [&>h1]:mb-2.5
-                        ${mode === 'dark' ? '[&>h1]:text-[#ff4d4d]' : '[&>h1]:text-black'}
+        {/* Four Editor  */}
+        <Editor
+          apiKey="zsnrfchd5qd7ri7ltirhv1lcglmq59yhtexly7ospxh3vq2d"
+          onEditorChange={(newValue, editor) => {
+            setBlogs({ ...blogs, content: newValue });
+            settext(editor.getContent({ format: "text" }));
+          }}
+          onInit={(evt, editor) => {
+            settext(editor.getContent({ format: "text" }));
+          }}
+          init={{
+            plugins:
+              "a11ychecker advcode advlist advtable anchor autocorrect autolink autoresize autosave casechange charmap checklist code codesample directionality editimage emoticons export footnotes formatpainter fullscreen help image importcss inlinecss insertdatetime link linkchecker lists media mediaembed mentions mergetags nonbreaking pagebreak pageembed permanentpen powerpaste preview quickbars save searchreplace table tableofcontents template  tinydrive tinymcespellchecker typography visualblocks visualchars wordcount",
+          }}
+        />
+
+        {/* Five Submit Button  */}
+        <Button
+          className=" w-full mt-5"
+            onClick={addPost}
+          style={{
+            background:
+              mode === "dark" ? "rgb(226, 232, 240)" : "rgb(30, 41, 59)",
+            color: mode === "dark" ? "rgb(30, 41, 59)" : "rgb(226, 232, 240)",
+          }}
+        >
+          Send
+        </Button>
+
+        {/* Six Preview Section  */}
+        <div className="">
+          <h1 className=" text-center mb-3 text-2xl">Preview</h1>
+          <div className="content">
+            <div
+              className={`[&> h1]:text-[32px] [&>h1]:font-bold  [&>h1]:mb-2.5
+                        ${
+                          mode === "dark"
+                            ? "[&>h1]:text-[#ff4d4d]"
+                            : "[&>h1]:text-black"
+                        }
 
                         [&>h2]:text-[24px] [&>h2]:font-bold [&>h2]:mb-2.5
-                        ${mode === 'dark' ? '[&>h2]:text-white' : '[&>h2]:text-black'}
+                        ${
+                          mode === "dark"
+                            ? "[&>h2]:text-white"
+                            : "[&>h2]:text-black"
+                        }
 
                         [&>h3]:text-[18.72] [&>h3]:font-bold [&>h3]:mb-2.5
-                        ${mode === 'dark' ? '[&>h3]:text-white' : '[&>h3]:text-black'}
+                        ${
+                          mode === "dark"
+                            ? "[&>h3]:text-white"
+                            : "[&>h3]:text-black"
+                        }
 
                         [&>h4]:text-[16px] [&>h4]:font-bold [&>h4]:mb-2.5
-                        ${mode === 'dark' ? '[&>h4]:text-white' : '[&>h4]:text-black'}
+                        ${
+                          mode === "dark"
+                            ? "[&>h4]:text-white"
+                            : "[&>h4]:text-black"
+                        }
 
                         [&>h5]:text-[13.28px] [&>h5]:font-bold [&>h5]:mb-2.5
-                        ${mode === 'dark' ? '[&>h5]:text-white' : '[&>h5]:text-black'}
+                        ${
+                          mode === "dark"
+                            ? "[&>h5]:text-white"
+                            : "[&>h5]:text-black"
+                        }
 
                         [&>h6]:text-[10px] [&>h6]:font-bold [&>h6]:mb-2.5
-                        ${mode === 'dark' ? '[&>h6]:text-white' : '[&>h6]:text-black'}
+                        ${
+                          mode === "dark"
+                            ? "[&>h6]:text-white"
+                            : "[&>h6]:text-black"
+                        }
 
                         [&>p]:text-[16px] [&>p]:mb-1.5
-                        ${mode === 'dark' ? '[&>p]:text-[#7efff5]' : '[&>p]:text-black'}
+                        ${
+                          mode === "dark"
+                            ? "[&>p]:text-[#7efff5]"
+                            : "[&>p]:text-black"
+                        }
 
                         [&>ul]:list-disc [&>ul]:mb-2
-                        ${mode === 'dark' ? '[&>ul]:text-white' : '[&>ul]:text-black'}
+                        ${
+                          mode === "dark"
+                            ? "[&>ul]:text-white"
+                            : "[&>ul]:text-black"
+                        }
 
                         [&>ol]:list-decimal [&>li]:mb-10
-                        ${mode === 'dark' ? '[&>ol]:text-white' : '[&>ol]:text-black'}
+                        ${
+                          mode === "dark"
+                            ? "[&>ol]:text-white"
+                            : "[&>ol]:text-black"
+                        }
 
                         [&>li]:list-decimal [&>ol]:mb-2
-                        ${mode === 'dark' ? '[&>ol]:text-white' : '[&>ol]:text-black'}
+                        ${
+                          mode === "dark"
+                            ? "[&>ol]:text-white"
+                            : "[&>ol]:text-black"
+                        }
 
                         [&>img]:rounded-lg
                         `}
-                         dangerouslySetInnerHTML={createMarkup(blogs.content)}>
-                    </div>
-            </div>
-        </div >
-            </div >
-        </div >
-    )
+              dangerouslySetInnerHTML={createMarkup(blogs.content)}
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default CreateBlog
+export default CreateBlog;
