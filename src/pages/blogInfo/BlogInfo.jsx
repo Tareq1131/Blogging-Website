2c6dc0fd3d48b92a9ed4d718e9c879fa33f76272
Tareq1131 <tareqislam076@gmail.com>
2024-04-28 02:35:18 +0600

    Comment Function In BlogInfo

--- src/pages/blogInfo/BlogInfo.jsx
+++ src/pages/blogInfo/BlogInfo.jsx
@@ -1,12 +1,13 @@
 import  { useContext, useEffect, useState } from 'react'
 import myContext from '../../context/data/myContext';
 import { useParams } from 'react-router';
+import { Timestamp, addDoc, collection, doc, getDoc, onSnapshot, orderBy, query } from 'firebase/firestore';
 
 import Layout from '../../components/layout/Layout';
 import Loader from '../../components/loader/Loader';
-import { doc, getDoc } from 'firebase/firestore';
+import Comment from '../../components/comment/Comment';
+import toast from 'react-hot-toast';
 import { fireDB } from '../../fairebase/FirebaseConfig';
-
 
 function BlogInfo() {
   const context = useContext(myContext);
@@ -47,6 +48,62 @@ function BlogInfo() {
   }
 
 
+  const [fullName, setFullName] = useState('');
+  const [commentText, setCommentText] = useState('');
+
+  const addComment = async () => {
+    const userRef = collection(fireDB, "blogPost/" + `${params.id}/` + "comment")
+    try {
+      await addDoc(
+        userRef, {
+        fullName,
+        commentText,
+        time: Timestamp.now(),
+        date: new Date().toLocaleString(
+          "en-US",
+          {
+            month: "short",
+            day: "2-digit",
+            year: "numeric",
+          }
+        )
+      })
+      toast.success('Comment Add Successfully');
+      setFullName("")
+      setCommentText("")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+
+  const [allComment, setAllComment] = useState([]);
+
+  const getcomment = async () => {
+    try {
+      const q = query(
+        collection(fireDb, "blogPost/" + `${params.id}/` + "comment/"),
+        orderBy('time')
+      );
+      const data = onSnapshot(q, (QuerySnapshot) => {
+        let productsArray = [];
+        QuerySnapshot.forEach((doc) => {
+          productsArray.push({ ...doc.data(), id: doc.id });
+        });
+        setAllComment(productsArray)
+        console.log(productsArray)
+      });
+      return () => data;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getcomment()
+  }, []);
+
   return (
     <Layout>
       <section className="rounded-lg h-full overflow-hidden max-w-4xl mx-auto px-4 ">
@@ -54,30 +111,30 @@ function BlogInfo() {
           {loading ?
             <Loader />
             :
-            <div >
-                {/* Thumbnail  */}
-                <img alt="content" className="mb-3 rounded-lg h-full w-full"
-                  src={getBlogs?.thumbnail}
-                />
-                {/* title And date  */}
-                <div className="flex justify-between items-center mb-3">
-                  <h1 style={{ color: mode === 'dark' ? 'white' : 'black' }}
-                    className=' text-xl md:text-2xl lg:text-2xl font-semibold'>
-                    {getBlogs?.blogs?.title}
-                  </h1>
-                  <p style={{ color: mode === 'dark' ? 'white' : 'black' }}>
-                    {getBlogs?.date}
-                  </p>
-                </div>
-                <div
-                  className={`border-b mb-5 ${mode === 'dark' ?
+            <div>
+              {/* Thumbnail  */}
+              <img alt="content" className="mb-3 rounded-lg h-full w-full"
+                src={getBlogs?.thumbnail}
+              />
+              {/* title And date  */}
+              <div className="flex justify-between items-center mb-3">
+                <h1 style={{ color: mode === 'dark' ? 'white' : 'black' }}
+                  className=' text-xl md:text-2xl lg:text-2xl font-semibold'>
+                  {getBlogs?.blogs?.title}
+                </h1>
+                <p style={{ color: mode === 'dark' ? 'white' : 'black' }}>
+                  {getBlogs?.date}
+                </p>
+              </div>
+              <div
+                className={`border-b mb-5 ${mode === 'dark' ?
                         'border-gray-600' : 'border-gray-400'}`}
-                />
+              />
 
-                {/* blog Content  */}
-                <div className="content">
-                  <div
-                    className={`[&> h1]:text-[32px] [&>h1]:font-bold  [&>h1]:mb-2.5
+              {/* blog Content  */}
+              <div className="content">
+                <div
+                className={`[&> h1]:text-[32px] [&>h1]:font-bold  [&>h1]:mb-2.5
                         ${mode === 'dark' ? '[&>h1]:text-[#ff4d4d]' : '[&>h1]:text-black'}
 
                         [&>h2]:text-[24px] [&>h2]:font-bold [&>h2]:mb-2.5
@@ -109,13 +166,20 @@ function BlogInfo() {
 
                         [&>img]:rounded-lg
                         `}
-                    dangerouslySetInnerHTML={createMarkup(getBlogs?.blogs?.content)}>
-
-                  </div>
+                  dangerouslySetInnerHTML={createMarkup(getBlogs?.blogs?.content)}>
+                </div>
               </div>
-
             </div>
           }
+
+          <Comment
+            addComment={addComment}
+            commentText={commentText}
+            setcommentText={setCommentText}
+            allComment={allComment}
+            fullName={fullName}
+            setFullName={setFullName}
+          />
 
         </div>
       </section>
